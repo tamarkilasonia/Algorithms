52d06c883083dda4129b5e7fbad9f6d5c9c30ce7
Killas <t.kilasonia2006@gmail.com>
2025-02-28 23:22:32 +0400

    Update QuickSort.java

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -16,7 +16,7 @@ public class QuickSort {
         int pivot = array[high];
         int i = (low - 1);
 
-        
+    
         for (int j = low; j < high; j++) {
             if (array[j] <= pivot) {
                 i++;
