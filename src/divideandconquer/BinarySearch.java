e0c5ca07a8621fe7307783cd7e2e25376a4b5b1a
tamro killas <t.kilasonia2006@gmail.com>
2025-03-01 12:57:33 +0400

    >

--- src/divideandconquer/BinarySearch.java
+++ src/divideandconquer/BinarySearch.java
@@ -1,5 +1,33 @@
 package divideandconquer;
 
 public class BinarySearch {
-  
+
+    // Binary Search function
+    public static int binarySearch(int[] arr, int left, int right, int target) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; // Element not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11, 13};
+        int target = 7;
+        int index = binarySearch(arr, 0, arr.length - 1, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
 }
+
--- src/divideandconquer/MergeSort.java
+++ src/divideandconquer/MergeSort.java
@@ -1,4 +1,60 @@
 package divideandconquer;
 
+import java.util.Arrays;
+
 public class MergeSort {
+
+    // Merge two sorted subarrays into one
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        System.arraycopy(arr, left, L, 0, n1);
+        System.arraycopy(arr, mid + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Recursive merge sort function
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Before Sorting: " + Arrays.toString(arr));
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println("After Sorting: " + Arrays.toString(arr));
+    }
 }
